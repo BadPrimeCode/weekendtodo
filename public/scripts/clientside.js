adac4422f0c2878e8ffb8dd7aaca2c2590da4644
Ian <shiny@what.local>
2016-09-17 18:12:11 -0500

    saving for posterity

--- public/scripts/clientside.js
+++ public/scripts/clientside.js
@@ -1,6 +1,20 @@
 console.log('client.js sourced');
 
+var todoList = [];
+
 $(document).ready(function() {
     console.log('document ready');
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
 }); //end document ready
